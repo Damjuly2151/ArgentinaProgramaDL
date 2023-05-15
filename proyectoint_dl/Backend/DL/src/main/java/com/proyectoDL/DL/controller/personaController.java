b5f4aaa7cc558bf7e4fc34c65b8cd6f03fc175b7
Damjuly2151 <damian_5121@yahoo.com.ar>
2023-05-15 16:34:17 -0300

    Back

---
+++ proyectoint_dl/Backend/DL/src/main/java/com/proyectoDL/DL/controller/personaController.java
@@ -0,0 +1,54 @@
+package com.proyectoDL.DL.controller;
+
+import com.proyectoDL.DL.Interface.IPersonaService;
+import com.proyectoDL.DL.entity.Persona;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class personaController {
+    @Autowired IPersonaService ipersonaService;
+    @GetMapping("persona/traer")
+    public List<Persona> getPersona(){
+        return ipersonaService.getPersona();
+    }
+    
+    @PostMapping("person/crear")
+    public String createPersona(@RequestBody Persona persona){
+        ipersonaService.savePersona(persona);
+        return "la persona fue creada correctamente";
+              
+    }
+    
+    @DeleteMapping("/persona/borrar/(Id)")
+    public String deletePersona(@PathVariable Long Id){
+        ipersonaService.deletePersona(Id);
+        return "la persona fue eliminada correctamente";
+    }
+    
+    @PutMapping("/persona/editar/(Id)")
+    public Persona editPersona(@PathVariable Long Id,
+            @RequestParam("nombre")String nuevoNombre,
+            @RequestParam("apellido")String nuevoApellido,
+            @RequestParam("img")String nuevoImg){
+        Persona persona = ipersonaService.findPersona(Id);
+        
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImg(nuevoImg);
+        
+        ipersonaService.savePersona(persona);
+        return persona;
+        
+    }
+    
+    
+} 
