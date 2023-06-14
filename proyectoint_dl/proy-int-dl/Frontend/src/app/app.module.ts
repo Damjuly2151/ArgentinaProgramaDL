37516ec9ccdbaae7705dabf89ea15a40aff50cf3
Damjuly2151 <damian_5121@yahoo.com.ar>
2023-06-14 13:10:25 -0300

    cambios

--- proyectoint_dl/proy-int-dl/Frontend/src/app/app.module.ts
+++ proyectoint_dl/proy-int-dl/Frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -10,7 +9,6 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 
@@ -23,7 +21,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent,
     ProyectoComponent,
     FooterComponent
   ],
@@ -32,8 +29,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppRoutingModule,
     NgCircleProgressModule.forRoot({})
 
-   }
-  
   ],
   providers: [],
   bootstrap: [AppComponent]
