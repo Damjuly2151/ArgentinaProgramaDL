2f3acc4c3d7162a3d330a935b8bbf0614e67a54f
Damjuly2151 <damian_5121@yahoo.com.ar>
2023-06-26 14:37:32 -0300

    Back

--- proyectoint_dl/Backend/src/main/java/com/portfolio/DL/Interface/IPersonaService.java
+++ proyectoint_dl/Backend/src/main/java/com/portfolio/DL/Interface/IPersonaService.java
@@ -1,10 +1,23 @@
 package com.portfolio.DL.Interface;
 
+import com.portfolio.DL.Entity.Persona;
+import java.util.List;
+
+
 
 public interface IPersonaService {
     //Traer una listas de personas
-    public List<Personas> getPersona();
+    public List<Persona> getPersona();
     
+    //Guardar un objeto tipo Persona
+    public void savePersona(Persona persona);
+            
+    //Eliminar un objeto pero lo buscamos ID
+    public void deletePersona(Long id);
+            
+    //Buscar persona por id
+    public Persona findPersona(Long id);         
+            
     
     
     
